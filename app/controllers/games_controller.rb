c03c3f41f84211bb0a39e227ff0c71ab2e21e656
Matt Gillooly <matt@swipely.com>
2012-02-28 12:30:23 -0500

    more data, styling, and less

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,30 @@
 class GamesController < ApplicationController
 
-  class Player < Struct.new(:html_class, :name, :you)
+  class Player < Struct.new(:html_class, :name, :available, :you)
     def to_partial_path
       'player'
     end
   end
 
-  def index
+  class OfflineHippo < Struct.new(:html_class)
+    def to_partial_path
+      'offline_hippo'
+    end
+  end
 
+  class AvailableHippo < Struct.new(:html_class)
+    def to_partial_path
+      'available_hippo'
+    end
+  end
+
+  class ChatEvent < Struct.new(:actor, :message)
+    def to_partial_path
+      'chat_event'
+    end
+  end
+
+  def index
   end
 
   def show
@@ -38,13 +55,52 @@ class GamesController < ApplicationController
 
     #- your game has ended
     #  - play again or join waitlist
-    
-    @players = [
-      Player.new('sweetie', 'Player 1', false),
-      Player.new('bottomless', 'Player 2', true),
-      Player.new('picky', 'Player 3', false),
-      Player.new('veggie', 'Player 4', false)
+   
+    @players = players_for_state(state) 
+
+    @chat_events = [
+      ChatEvent.new('Matt', "Who's ready to get their ass kicked?"),
+      ChatEvent.new('Steve', "Bring it on!"),
+      ChatEvent.new('Carol', "Ha! You wish!"),
+      ChatEvent.new('Judy', "You guys suck"),
+      ChatEvent.new('Steve', "Judy, you're mean.")
     ]
+  end
+
+private
+
+  def players_for_state(state)
+    case state
+    when :offline
+      [
+        OfflineHippo.new('sweetie'),
+        OfflineHippo.new('bottomless'),
+        OfflineHippo.new('picky'),
+        OfflineHippo.new('veggie')
+      ]
+    when :full
+      [
+        Player.new('sweetie', 'Judy'),
+        Player.new('bottomless', 'Matt'),
+        Player.new('picky', 'Steve'),
+        Player.new('veggie', 'Carol')
+      ]
+    when :alone
+      [
+        AvailableHippo.new('sweetie'),
+        AvailableHippo.new('bottomless'),
+        AvailableHippo.new('picky'),
+        AvailableHippo.new('veggie')
+      ]
+    else
+      # free_hippo, playing, game_over
+      [
+        Player.new('sweetie', 'Judy'),
+        Player.new('bottomless', 'Matt', false, true),
+        Player.new('picky', 'Steve'),
+        AvailableHippo.new('veggie')
+      ]
+    end
   end
 
 end
