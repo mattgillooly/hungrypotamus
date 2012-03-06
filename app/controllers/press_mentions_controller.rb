261c7f8dd3fda981d3fa96e7e4902cd2db6ecff2
Matt Gillooly <matt@swipely.com>
2012-03-05 23:43:35 -0500

    adding press page

---
+++ app/controllers/press_mentions_controller.rb
@@ -0,0 +1,3 @@
+class PressMentionsController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Hungrypotamus::Application.routes.draw do
   resources :games, :only => [:index, :show]
   resources :bites, :only => [:create]
   resources :gloats, :only => [:create]
+  resources :press_mentions, :only => [:index]
 
   root :to => 'games#index'
 
