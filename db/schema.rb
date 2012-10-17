a088f8ddf21141b43a6533570e855adfe497e062
Matt Gillooly <matt@mattgillooly.com>
2012-10-17 06:19:22 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120306063228) do
+
+  create_table "chat_events", :force => true do |t|
+    t.string   "actor"
+    t.string   "message"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "chat_events", ["created_at"], :name => "index_chat_events_on_created_at"
 
 end
